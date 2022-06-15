58069ef98fd8478466e7a3dd3cca53192b0afda1
felipemcs <felipemcs@dcomp.ufs.br>
2022-06-15 13:30:00 +0100

    Adds slack notifications

--- src/Message/CommentMessage.php
+++ src/Message/CommentMessage.php
@@ -4,17 +4,24 @@ namespace App\Message;
 class CommentMessage
 {
     private $id;
+    private $reviewUrl;
     private $context;
 
-    public function __construct(int $id, array $context = [])
+    public function __construct(int $id, string $reviewUrl, array $context = [])
     {
         $this->id = $id;
         $this->context = $context;
+        $this->reviewUrl = $reviewUrl;
     }
 
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function getReviewUrl(): string
+    {
+        return $this->reviewUrl;
     }
 
     public function getContext(): array
